9964a0ec592f62113236e24c2aee5af5100f0250
Vladimír Bobeš Tužinský <vladimir@tuzinsky.com>
2012-07-07 14:49:05 +0200

    Tiny font-face fix

--- app.rb
+++ app.rb
@@ -98,6 +98,7 @@ end
 
 def sprockets
   @sprockets ||= Sprockets::Environment.new.tap do |environment|
+    environment.append_path "assets/fonts"
     environment.append_path "assets/images"
     environment.append_path "assets/javascripts"
     environment.append_path "assets/stylesheets"
